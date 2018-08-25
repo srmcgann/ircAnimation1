02cfaeb1e4c8baceee59dd2e457f688b3982d85b
Scott <smcgann@cantelope.ml>
2018-08-25 16:02:59 -0700

    initial commit

---
+++ game.js
@@ -0,0 +1,64 @@
+(() => {
+  Draw = () => {
+    x.globalAlpha = .25
+    x.fillRect(0, 0, c.width, c.height)
+    x.globalAlpha = 1
+    let X = 0
+    let Y = 0
+    let ox = -81
+    let oy = 0
+    let v = t * 1.5
+    let segs = 20
+    let radius = 55
+    let segLength = .1 + Math.pow(.55 + S(v) / 2, 25)
+    if( segLength > .1 + Math.pow(.55 + .995 / 2, 25)) circles.push({X:cx+ox, Y:cy+oy, radius})
+    for (let j = 0; j < segs; ++j) {
+      let r = radius + segLength * j
+      for (let i = 0; i < 3; ++i) {
+        x.beginPath()
+        x.strokeStyle = `hsla(${360 / segs * j  - t * 1e3}, 50%, 80%, ${j / segs * segLength / 2})`
+        x.lineWidth = (segs - j) / segs * 99
+        let p = Math.PI * 2 / 3 * i - v + C(v) - j / 20 * C(v)
+        X = cx + S(p) * r + ox
+        Y = cy + C(p) * r + oy
+        x.moveTo(X, Y)
+        X = cx + S(p) * (r + segLength) + ox
+        Y = cy + C(p) * (r + segLength) + oy
+        x.lineTo(X, Y)
+        x.stroke()
+      }
+    }
+    for(let i = 0; i < circles.length; ++i) {
+      x.lineWidth = circles[i].radius / 6
+      x.beginPath()
+      x.strokeStyle = `hsla(${360 / circles.length * i  - t * 400}, 99%, 50%, ${.6 / (circles[i].radius / 8)})`
+      x.arc(circles[i].X, circles[i].Y, circles[i].radius, 0, 7)
+      x.stroke()
+      circles[i].radius += circles[i].radius / 50
+      if(circles[i].radius > 2e3) circles.splice(i, 1)
+    }
+    x.drawImage(logo, cx - logo.width / 2, cy - logo.height / 2)
+    
+    t+=1/60
+    requestAnimationFrame(Draw);
+  }
+  let c=document.getElementById("canvas")
+  let x=c.getContext("2d")
+  c.width = c.clientWidth
+  c.height = c.clientHeight
+  let S = Math.sin
+  let C = Math.cos
+  let t = 0
+  let cx = c.width / 2
+  let cy = c.height / 2
+  let circles = []
+  window.addEventListener('resize', e => {
+    c.width = c.clientWidth
+    c.height = c.clientHeight
+    cx = c.width / 2
+    cy = c.height / 2
+  })
+  let logo = new Image()
+  logo.onload = e => { Draw() }
+  logo.src = 'irc.png'
+})()
